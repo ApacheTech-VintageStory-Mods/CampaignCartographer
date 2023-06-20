a7a3c88a7c03661d5317f1b62ae1d2c63cd2a5cf
ApacheTech Solutions <p_matthews@live.co.uk>
2023-06-20 23:01:28 +0100

    feat(GPS): add default command handler
    [GPS] The user should be able to display their current XYZ position.

--- src/CampaignCartographer/Features/GPS/Systems/GpsClientSystem.cs
+++ src/CampaignCartographer/Features/GPS/Systems/GpsClientSystem.cs
@@ -8,10 +8,28 @@
 ///     [GPS] The user should be able to send their current XYZ position to other players, as a clickable link that sets a waypoint on their map.
 /// </summary>
 /// <seealso cref="ClientModSystem" />
-/// <seealso cref="IClientServiceRegistrar" />
-internal class GpsClientSystem : ClientModSystem, IClientServiceRegistrar
+[UsedImplicitly]
+internal class GpsClientSystem : ClientModSystem
 {
-    public void ConfigureClientModServices(IServiceCollection services, ICoreClientAPI capi)
+    public override void StartClientSide(ICoreClientAPI capi)
     {
+        capi.ChatCommands
+            .Create("gps")
+            .WithDescription(LangEx.FeatureString("GPS", "ClientCommandDescription"))
+            .HandleWith(OnClientDefaultHandler);
+    }
+
+    private static TextCommandResult OnClientDefaultHandler(TextCommandCallingArgs args)
+    {
+        var pos = PlayerLocationMessage(args.Caller.Player);
+        return TextCommandResult.Success(pos);
+    }
+    
+    private static string PlayerLocationMessage(IPlayer player)
+    {
+        var pos = player.Entity.Pos.AsBlockPos;
+        var displayPos = pos.RelativeToSpawn();
+        var message = $"X = {displayPos.X}, Y = {displayPos.Y}, Z = {displayPos.Z}.";
+        return message;
     }
 }
