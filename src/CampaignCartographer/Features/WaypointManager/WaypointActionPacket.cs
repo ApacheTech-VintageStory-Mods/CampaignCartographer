2d9d8c8eb4e0057e22d121269d8a6b9b8cf5c259
ApacheTech Solutions <p_matthews@live.co.uk>
2025-03-07 18:31:43 +0000

    feat: add waypoint add/edit network handlers

---
+++ src/CampaignCartographer/Features/WaypointManager/WaypointActionPacket.cs
@@ -0,0 +1,19 @@
+﻿using Gantry.Core.GameContent.GUI.Abstractions;
+
+namespace ApacheTech.VintageMods.CampaignCartographer.Features.WaypointManager;
+
+/// <summary>
+///     Represents a network packet for performing actions on a waypoint.
+/// </summary>
+public class WaypointActionPacket
+{
+    /// <summary>
+    ///     The waypoint associated with the action.
+    /// </summary>
+    public Waypoint Waypoint { get; set; }
+
+    /// <summary>
+    ///     The mode indicating whether the action is an addition or an edit.
+    /// </summary>
+    public AddEditDialogueMode Mode { get; set; }
+}
