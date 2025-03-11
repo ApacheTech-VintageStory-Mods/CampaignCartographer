c58b87ff8c111daf7886d466c530c983a34b201c
ApacheTech Solutions <p_matthews@live.co.uk>
2025-03-11 02:13:18 +0000

    fix (#52): inject capi into patch and validate

--- src/CampaignCartographer/Features/WaypointGroups/Patches/WorldMapManagerPatches.cs
+++ src/CampaignCartographer/Features/WaypointGroups/Patches/WorldMapManagerPatches.cs
@@ -6,7 +6,9 @@ using Vintagestory.API.Util;
 
 namespace ApacheTech.VintageMods.CampaignCartographer.Features.WaypointGroups.Patches;
 
-[HarmonyClientSidePatch]
+// In singleplayer, this patch also runs on the server. They use the same event hook.
+// Also patch on server, so that it gets explicitly unpatched on dispose.
+[HarmonyUniversalPatch]
 public class WorldMapManagerPatches : WorldSettingsConsumer<WaypointGroupsSettings>
 {
     /// <summary>
@@ -15,24 +17,22 @@ public class WorldMapManagerPatches : WorldSettingsConsumer<WaypointGroupsSettin
     [HarmonyPrefix]
     [HarmonyPriority(Priority.First)]
     [HarmonyPatch(typeof(WorldMapManager), "OnLvlFinalize")]
-    public static bool Harmony_WorldMapManager_OnLvlFinalize_Prefix(WorldMapManager __instance, ref Thread ___mapLayerGenThread, GuiDialogWorldMap ___worldMapDlg)
+    public static bool Harmony_WorldMapManager_OnLvlFinalize_Prefix(WorldMapManager __instance, ICoreClientAPI ___capi, ref Thread ___mapLayerGenThread, GuiDialogWorldMap ___worldMapDlg)
     {
-        // In singleplayer, this patch also runs on the server. They use the same instance.
-        var capi = ApiEx.Client;
-        if (capi is null) return true;
-        var mapAllowedClient = capi.World.Config.GetBool("allowMap", true) || capi.World.Player.Privileges.IndexOf("allowMap") != -1;
+        if (___capi is null) return true;
+        var mapAllowedClient = ___capi.World.Config.GetBool("allowMap", true) || ___capi.World.Player.Privileges.IndexOf("allowMap") != -1;
         if (mapAllowedClient)
         {
             ApiEx.Logger.VerboseDebug("Registering world map hotkeys");
-            capi.Input.RegisterHotKey("worldmaphud", Lang.Get("Show/Hide Minimap"), GlKeys.F6, HotkeyType.HelpAndOverlays);
-            capi.Input.RegisterHotKey("minimapposition", Lang.Get("keycontrol-minimap-position"), GlKeys.F6, HotkeyType.HelpAndOverlays, false, true, false);
-            capi.Input.RegisterHotKey("worldmapdialog", Lang.Get("Show World Map"), GlKeys.M, HotkeyType.HelpAndOverlays);
-            capi.Input.SetHotKeyHandler("worldmaphud", key => __instance.CallMethod<bool>("OnHotKeyWorldMapHud", key));
-            capi.Input.SetHotKeyHandler("minimapposition", key => __instance.CallMethod<bool>("OnHotKeyMinimapPosition", key));
-            capi.Input.SetHotKeyHandler("worldmapdialog", key => __instance.CallMethod<bool>("OnHotKeyWorldMapDlg", key));
+            ___capi.Input.RegisterHotKey("worldmaphud", Lang.Get("Show/Hide Minimap"), GlKeys.F6, HotkeyType.HelpAndOverlays);
+            ___capi.Input.RegisterHotKey("minimapposition", Lang.Get("keycontrol-minimap-position"), GlKeys.F6, HotkeyType.HelpAndOverlays, false, true, false);
+            ___capi.Input.RegisterHotKey("worldmapdialog", Lang.Get("Show World Map"), GlKeys.M, HotkeyType.HelpAndOverlays);
+            ___capi.Input.SetHotKeyHandler("worldmaphud", key => __instance.CallMethod<bool>("OnHotKeyWorldMapHud", key));
+            ___capi.Input.SetHotKeyHandler("minimapposition", key => __instance.CallMethod<bool>("OnHotKeyMinimapPosition", key));
+            ___capi.Input.SetHotKeyHandler("worldmapdialog", key => __instance.CallMethod<bool>("OnHotKeyWorldMapDlg", key));
 
             ApiEx.Logger.VerboseDebug("Registering world map link protocols");
-            capi.RegisterLinkProtocol("worldmap", link => __instance.CallMethod("onWorldMapLinkClicked", link));
+            ___capi.RegisterLinkProtocol("worldmap", link => __instance.CallMethod("onWorldMapLinkClicked", link));
         }
 
         lock (MapLayerGeneration.MapLayersLock)
@@ -40,7 +40,7 @@ public class WorldMapManagerPatches : WorldSettingsConsumer<WaypointGroupsSettin
             ApiEx.Logger.VerboseDebug("Creating map layer instances");
             foreach (var (key, value) in __instance.MapLayerRegistry)
             {
-                if (key == "entities" && !capi.World.Config.GetAsBool("entityMapLayer")) continue;
+                if (key == "entities" && !___capi.World.Config.GetAsBool("entityMapLayer")) continue;
                 ApiEx.Logger.VerboseDebug($" - {key}: {value.Name}");
                 var instance = ActivatorUtilities.CreateInstance(IOC.Services, value, __instance).To<MapLayer>();
                 __instance.MapLayers.Add(instance);
@@ -73,7 +73,7 @@ public class WorldMapManagerPatches : WorldSettingsConsumer<WaypointGroupsSettin
         ApiEx.Logger.VerboseDebug("Starting map layer generation thread");
         ___mapLayerGenThread.Start();
 
-        if (capi is not null && (capi.Settings.Bool["showMinimapHud"] || !capi.Settings.Bool.Exists("showMinimapHud"))
+        if (___capi is not null && (___capi.Settings.Bool["showMinimapHud"] || !___capi.Settings.Bool.Exists("showMinimapHud"))
         && (___worldMapDlg is null || !___worldMapDlg.IsOpened()))
         {
             ApiEx.Logger.VerboseDebug("Opening mini-map");
